8c0cf13a3d02cd6f7350d6f09e7b7e9332521d65
blastcraftmc <scolberg@proton.me>
2023-03-08 20:39:09 -0500

    Music

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 //Songs
-const sOne = {song:"Starboy", artist:"The Weeknd, Daft Punk"};
-const sTwo = {song:"Bandit", artist:"Juice WRLD, YoungBoy Never Broke Again"};
-const sThree = {song:"Creepin'", artist:"Metro Boomin, The Weeknd, 21 Savage"};
+const sOne = {song:"Starboy", artist:"The Weeknd, Daft Punk", audio:"audio/Starboy.mp3"};
+const sTwo = {song:"Bandit", artist:"Juice WRLD, YoungBoy Never Broke Again", audio:"audio/Bandit.mp3"};
+const sThree = {song:"Creepin'", artist:"Metro Boomin, The Weeknd, 21 Savage", audio:"audio/Creepin.mp3"};
 
 const songs = [sOne, sTwo, sThree]
 
 //Main
-    let failCount = 0;
-
+    let count = 0
     const random = Math.floor(Math.random() * songs.length);
     console.log(random, songs[random]);
 
     let songTitle = songs[random].song;
     let songArtist = songs[random].artist;
+    var songTrack = songs[random].audio;
 
     console.log(songTitle, songArtist);
 
@@ -21,12 +21,39 @@ function itemSelected(selectedSongTitle, selectedSongArtist) {
         var status = true;
     } else {
         var status = false;
-        failCount++;
+        populate(selectedSongTitle, selectedSongArtist)
     }
 }
 
-function populate() {
+function populate(selectedSongTitle, selectedSongArtist) {
+    //Creating Div
+    let imageWrapper = document.createElement("div");
+    imageWrapper.classList.add("imageWrapper");
 
+    //Image (img)
+    var rowImg = new Image();
+    rowImg.src = "images/wrong.png";
+    rowImg.classList.add("image");
+
+    //p
+	let rowName = document.createElement("p");
+    let concatSongName = selectedSongTitle + ", " + selectedSongArtist
+    rowName.appendChild(document.createTextNode(concatSongName));
+    rowName.classList.add("text");
+
+    //Append
+    imageWrapper.appendChild(rowImg);
+	imageWrapper.appendChild(rowName);
+
+    let allNamesElm = document.getElementById(count);
+
+    allNamesElm.appendChild(imageWrapper);
+    count++
+
+    if (count > 5) {
+        console.log("Game Lost")
+        endGame(false)
+    }
 }
 
 document.getElementById("textBox").addEventListener('input', function (evt) {
@@ -41,3 +68,18 @@ document.getElementById("textBox").addEventListener('input', function (evt) {
         document.getElementById("slideUp").style.display = "none";
     }
 });
+
+function endGame(condition) {
+    if (condition == true) {
+        window.alert("You Won!");
+    } else if (condition == false) {
+        window.alert("You Lost!");
+    } else {
+        window.alert("Error");
+    }
+}
+
+function playAudio() {
+    let audio = new Audio(songTrack);
+    audio.play();
+}
